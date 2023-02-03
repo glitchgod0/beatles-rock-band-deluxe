890ab32d7e127b7a7de9b034f720b7d227eb03e3
Luna <luna.abarta@gmail.com>
2023-02-02 17:49:30 -0800

    patchcreator

--- _build_xbox.bat
+++ _build_xbox.bat
@@ -1,22 +1,12 @@
-set FAILED_ARK_BUILD=0
 git pull https://github.com/hmxmilohax/beatles-rock-band-deluxe main
-@echo off
-echo:
-echo:Temporarily moving PS3 files out of the ark path to reduce final ARK size
-@%SystemRoot%\System32\robocopy.exe "%~dp0_ark" "%~dp0_temp\_ark" *.milo_ps3 /S /MOVE /XD "%~dp0_temp\_ark" /NDL /NFL /NJH /NJS /R:0 >nul
-@%SystemRoot%\System32\robocopy.exe "%~dp0_ark" "%~dp0_temp\_ark" *.png_ps3 /S /MOVE /XD "%~dp0_temp\_ark" /NDL /NFL /NJH /NJS /R:0 >nul
-@%SystemRoot%\System32\robocopy.exe "%~dp0_ark" "%~dp0_temp\_ark" *.bmp_ps3 /S /MOVE /XD "%~dp0_temp\_ark" /NDL /NFL /NJH /NJS /R:0 >nul
-echo:
-echo:Building Xbox ARK
-"%~dp0dependencies/arkhelper" dir2ark "%~dp0_ark" "%~dp0_build\xbox\gen" -n "patch_xbox" -e -v 5 >nul
-if %errorlevel% neq 0 (set FAILED_ARK_BUILD=1)
-echo:
-echo:Moving back PS3 files
-@%SystemRoot%\System32\robocopy.exe "%~dp0_temp\_ark" "%~dp0_ark" *.milo_ps3 /S /MOVE /XD "%~dp0_ark" /NDL /NFL /NJH /NJS /R:0 >nul
-@%SystemRoot%\System32\robocopy.exe "%~dp0_temp\_ark" "%~dp0_ark" *.png_ps3 /S /MOVE /XD "%~dp0_ark" /NDL /NFL /NJH /NJS /R:0 >nul
-@%SystemRoot%\System32\robocopy.exe "%~dp0_temp\_ark" "%~dp0_ark" *.bmp_ps3 /S /MOVE /XD "%~dp0_ark" /NDL /NFL /NJH /NJS /R:0 >nul
-rd _temp
-if %FAILED_ARK_BUILD% neq 1 (echo:Successfully built Beatles Rock Band Deluxe ARK. You may find the files needed to place on your Xbox 360 in /_build/Xbox/)
-if %FAILED_ARK_BUILD% neq 0 (echo:Error building ARK. Check your modifications or run _git_reset.bat to rebase your repo.)
-echo:
+@echo OFF
+echo:Make sure your 1.0 vanilla ark files are in _build/Xbox/gen/
+del "%~dp0\_build\Xbox\gen\main_xbox.hdr"
+del "%~dp0\_build\Xbox\gen\main_xbox_2.ark"
+xcopy "%~dp0\_build\_rebuild_files\main_xbox.hdr" "%~dp0\_build\Xbox\gen" >nul
+echo:Building Beatles Rock Band Deluxe patch arks.
+echo:The "Unhandled exception" below is expected, and does not indicate failure.
+"%~dp0dependencies/arkhelper" patchcreator -a "%~dp0\_ark" -o "%~dp0\_build\Xbox" "%~dp0\_build\Xbox\gen\main_xbox.hdr" >nul
+echo:Wrote Beatles Rock Band Deluxe patch arks.
+echo:Complete. Enjoy Beatles Rock Band Deluxe
 pause
